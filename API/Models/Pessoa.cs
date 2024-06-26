6bb8d772c1b7344346fc272bb90711532539fd16
Vinicius <vinicius_atanasio@hotmail.com>
2024-06-26 11:41:17 -0300

    feat: criacao de classes Models Pessoa e Telefone

---
+++ API/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Models;
+
+public class Pessoa
+
+{
+    public int PessoaId { get; set; }
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public bool EstaAtivo { get; set; }
+
+    // Relação de um para muitos com Telefone
+    public ICollection<Telefone> Telefones { get; set; }
+}
+
---
+++ API/Models/Telefone.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+
+namespace API;
+
+public class Telefone
+{
+    public int TelefoneId { get; set; }
+    public string Tipo { get; set; } // Celular, Residencial, Comercial
+    public string Numero { get; set; }
+
+    // Chave estrangeira para a classe Pessoa
+    public int PessoaId { get; set; }
+    public Pessoa Pessoa { get; set; }
+}
