0fdd03a0fbd2a38d43115ce28c0e9313de1c5962
Vinicius <vinicius_atanasio@hotmail.com>
2024-06-26 12:14:34 -0300

    feat: inserido Classe DbContext, para criacao de migracoes e banco de dados

--- API/Data/AppDataContext.cs
+++ API/Data/AppDataContext.cs
@@ -1,6 +1,30 @@
-﻿namespace API;
+﻿using Microsoft.EntityFrameworkCore;
+using API.Models;
 
-public class AppDataContext
+namespace API.Data;
+
+
+public class AppDataContext : DbContext
 {
+    public AppDataContext(DbContextOptions<AppDataContext> options) : base(options){}
 
-}
+    //implementado projeto em vida longa, com adicao de banco de dados
+    //utilizacao do EntityFramework para geracao de BD
+    
+    // Abaixo as Classes que vao virar tabelas no Banco de Dados
+    public DbSet<Pessoa> Pessoas { get; set; }
+    public DbSet<Telefone> Telefones { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //utilizando model builder para a definicao das classes e chaves primarias e estrangeiras
+        modelBuilder.Entity<Pessoa>()
+            .HasMany(p => p.Telefones)
+            .WithOne(t => t.Pessoa)
+            .HasForeignKey(t => t.PessoaId);
+    }
+
+
+    }
