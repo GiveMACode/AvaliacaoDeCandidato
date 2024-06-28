2baad1f3fe62ded50b7a04263b125d68fd3647d6
Vinicius <vinicius_atanasio@hotmail.com>
2024-06-28 19:26:21 -0300

    feat: PessoaService - ValidarDataNascimento, funcao de validacao

---
+++ API/Services/PessoaService.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Services;
+
+public class PessoaService : ServiceCollection
+{
+    public class ValidarDataNascimentoAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        DateTime dataNascimento = (DateTime)value;
+
+        if (dataNascimento > DateTime.Now)
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+
+        return ValidationResult.Success;
+    }
+}
+
+}
