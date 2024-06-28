5415739bb8f9a348d1429a463512e67130b2385e
Vinicius <vinicius_atanasio@hotmail.com>
2024-06-28 20:11:36 -0300

    feat: tratamento de requisicao CPF

--- API/Models/Pessoa.cs
+++ API/Models/Pessoa.cs
@@ -20,18 +20,26 @@ public class Pessoa
     [MaxLength(256, ErrorMessage = "O nome deve ter no máximo 256 caracteres!")]
     [Required(ErrorMessage = "Campo Obrigatório")]
     public string Nome { get; set; }
-    //tratamento de requisicao
-    [Required(ErrorMessage = "Campo Obrigatório")]
+    
+    
+    //tratamento de CPF
+    [Required(ErrorMessage = "O CPF é obrigatório.")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "O CPF deve conter exatamente 11 dígitos.")]
+    [ValidarCPF(ErrorMessage = "CPF inválido.")]
     public string CPF { get; set; }
-
+    //
     [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
     [ValidarDataNascimento(ErrorMessage = "A data de nascimento não pode ser no futuro.")]
     public DateTime DataNascimento { get; set; }
+
+
     public bool EstaAtivo { get; set; }
+
     // Relação de um para muitos com Telefone
     public ICollection<Telefone> Telefones { get; set; }
+
 
     //metodo para atualizacao de informacoes da classe Pessoa s/ Telefone
     public void Atualizar(string? nome, string? cpf, DateTime dataNascimento, Telefone telefone)
