1259ebc82e7d83f6ca5030a3b36dc40ae80cdf01
Vinicius <vinicius_atanasio@hotmail.com>
2024-06-28 17:08:48 -0300

    feat: condicional ListarPessoasAtivas

--- API/Controllers/PessoaController.cs
+++ API/Controllers/PessoaController.cs
@@ -2,37 +2,48 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata;
+using API.Models;
 
 namespace API.Controllers;
 
-    [Route("api/pessoa")]
-    [ApiController]
+[Route("api/pessoa")]
+[ApiController]
 public class PessoaController : ControllerBase
 {   //instancia do appcontext
     private readonly AppDataContext _context;
-   
-   // private readonly IMapper _mapper;
+
+    public PessoaController(AppDataContext context)
+    {
+        _context = context;
+    }
+
+    // private readonly IMapper _mapper;
 
     //api/pessoa GET
     [HttpGet]
     public IActionResult ListarPessoasAtivas()
+    {
+        var listaAtivos = _context.Pessoas.Where(d => d.EstaAtivo).ToList();
+        if (listaAtivos == null)
         {
-            var listaAtivos = _context.Pessoas.Where(d => d.EstaAtivo).ToList();
-
-            return Ok(listaAtivos);
+            return NotFound("No Active people found");
         }
+        return Ok(listaAtivos);
+    }
 
     //api/pessoa/72345678-l234-1234-1234-1234-12E4g6789i1  GET
     [HttpGet("{id}")]
     public IActionResult ListarPorId(Guid id)
     {
         var listaAtivos = _context.Pessoas.Include(t => t.Telefones).SingleOrDefault(d => d.Id == id);
-    
-        if(ListarPorId == null)
+
+        if (ListarPorId == null)
         {
             return NotFound();
         }
         return Ok(listaAtivos);
     }
+
+   
 
 }
