de320b3e180aa9f70b85b718c2c54b841f80610e
Vinicius <vinicius_atanasio@hotmail.com>
2024-06-28 21:22:35 -0300

    feat: cricacao de TelefoneService, Validacao de celular,  tipos de telefone

--- API/Models/Telefone.cs
+++ API/Models/Telefone.cs
@@ -1,14 +1,29 @@
 ﻿using API.Models;
+using API.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace API;
 
 public class Telefone
 {
     public Guid Id { get; set; }
-    public string Tipo { get; set; } // Celular, Residencial, Comercial
-    public string Numero { get; set; }
+
+    [Required(ErrorMessage = "O tipo de telefone é obrigatório.")]
+    public TipoTelefone Tipo { get; set; } // Celular, Residencial, Comercial
+    
+    [Required(ErrorMessage = "O número de telefone é obrigatório.")]
+    [PhoneValidation(ErrorMessage = "O formato do número de telefone é inválido.")]
+     public string Numero { get; set; }
 
     // Chave estrangeira para a classe Pessoa
     public Guid PessoaId { get; set; }
     public Pessoa Pessoa { get; set; }
+}
+
+
+public enum TipoTelefone
+{
+    Residencial,
+    Comercial,
+    Celular
 }
---
+++ API/Services/TelefoneService.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Services;
+
+public class TelefoneService : ServiceCollection
+{
+}
+public class PhoneValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var contato = (Telefone)validationContext.ObjectInstance;
+
+        if (contato.Numero == null)
+            return new ValidationResult("O número de telefone é obrigatório.");
+
+        string pattern = contato.Tipo switch
+        {
+            TipoTelefone.Residencial => @"^\(\d{2}\) \d{4}-\d{4}$",  // Exemplo: (12) 1234-5678
+            TipoTelefone.Comercial => @"^\(\d{2}\) \d{4}-\d{4}$",     // Exemplo: (12) 1234-5678
+            TipoTelefone.Celular => @"^\(\d{2}\) \d{5}-\d{4}$",       // Exemplo: (12) 91234-5678
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        if (!System.Text.RegularExpressions.Regex.IsMatch(contato.Numero, pattern))
+            return new ValidationResult(ErrorMessage);
+
+        return ValidationResult.Success;
+    }
+}
+
