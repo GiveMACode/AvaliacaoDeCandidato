6204ea099ba8ffd4dd4e3afa995eb323e416f557
Vinicius <vinicius_atanasio@hotmail.com>
2024-07-01 12:56:38 -0300

    feat: Usuario.cs Models

---
+++ API/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+public class Usuario
+{
+    public Guid id { get; set; }
+    
+    [MinLength(6, ErrorMessage = "O username deve ter no mínimo 6 caracteres!")]
+    [MaxLength(16, ErrorMessage = "O username deve ter no máximo 16 caracteres!")]
+    [Required(ErrorMessage = "Campo Obrigatório")]
+    public string NomeUsuario { get; set; }
+
+    [Required(ErrorMessage = "O campo Email é obrigatório.")]
+    [EmailAddress(ErrorMessage = "O campo Email não é um endereço de email válido.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "O campo Senha é obrigatório.")]
+    [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6 caracteres.")]
+    [MaxLength(16, ErrorMessage = "A senha não pode ter mais de 16 caracteres.")]
+    public string Senha { get; set; }
+
+    public Guid PessoaId { get; set; }
+    public Pessoa Pessoa { get; set; }
+}
